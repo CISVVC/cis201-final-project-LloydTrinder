6492829728aeae64ef90e259fad820dde2b06ea9
LloydTrinder <31451990+LloydTrinder@users.noreply.github.com>
2017-12-15 12:25:42 -0800

    Update word.h

--- word.h
+++ word.h
@@ -1,1 +1,22 @@
+#ifndef WORD_H
+#define WORD_H
+#include<vector>
+#include<string>
 
+class Word
+{
+public:
+   Word(std::string  word);
+   void add_line(int line);
+   void add_count(int count);
+   std::string get_word();
+   int get_count();
+   std::string get_lines();
+private:
+   int m_count;
+   std::string m_word;
+   std::vector<int> m_line;
+};
+
+
+#endif
