a5bf1b897fda19bcfa90451592fba1203b75a530
LloydTrinder <31451990+LloydTrinder@users.noreply.github.com>
2017-12-15 12:23:30 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+// concordance.cpp : Defines the entry point for the console application.
+//
+#include "concordance.h"
 
+
+
+
+int main()
+{
+	Concordance con = new Concordance("alice.txt");
+	con.parse();
+	con.print();
+
+	return 0;
+}
