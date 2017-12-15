98cd5aa67f45f1be89729f817513fb559ec8eeb3
LloydTrinder <31451990+LloydTrinder@users.noreply.github.com>
2017-12-15 12:25:06 -0800

    Update word.cpp

--- word.cpp
+++ word.cpp
@@ -1,1 +1,43 @@
+#include "word.h"
 
+
+Word::Word(std::string  word)
+{
+   m_word = word;
+   m_count = 1;
+}
+
+//
+
+std::string Word::get_word() 
+{ 
+	return (m_word); 
+}
+
+int Word::get_count()
+{
+	return(m_count);
+}
+
+
+std::string Word::get_lines()
+{
+	std::string lines;
+	for (int i = 0; i < m_line.size(); i++)
+	{
+		lines += (m_line[i] + ", ");
+	}
+	return (lines);
+}
+
+
+
+void Word::add_count(int count)
+{
+   m_count++;
+}
+
+void Word::add_line(int line)
+{
+   m_line.push_back(line);
+}
