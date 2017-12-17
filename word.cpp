d1ccc4e511bd262f38416b3818c4486dc5d608bf
LloydTrinder <31451990+LloydTrinder@users.noreply.github.com>
2017-12-16 17:50:53 -0800

    Update word.cpp

--- word.cpp
+++ word.cpp
@@ -1,3 +1,8 @@
+/* Lloyd Trinder
+** trinderl@student.vvc.edu
+** Professor tonning, cis 201
+** Develop a concordance generator program.  The concordance will be a list of all the words found in a text file with a count of the number of times the word occurred and a comma delimited list of the line numbers of where  the word occurred.  For each line of text, all punctuation must be removed and all letters in the string forced to lowercase.  Then, separate out the words and check each word against your list.   Keep track of the current line number and check if the word has been found before.  If the word has not been seen before, put it in some sort of collection data structure (an array or vector for example) and keep track of the line number.  If the word has been found before, store the line number of where the word occurred.*/ 
+
 #include "word.h"
 
 
@@ -11,12 +16,12 @@ Word::Word(std::string  word)
 
 std::string Word::get_word() 
 { 
-	return (m_word); 
+	return m_word; 
 }
 
 int Word::get_count()
 {
-	return(m_count);
+	return m_count;
 }
 
 
@@ -27,7 +32,7 @@ std::string Word::get_lines()
 	{
 		lines += (m_line[i] + ", ");
 	}
-	return (lines);
+	return lines;
 }
 
 
